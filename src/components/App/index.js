8c9527a301d28f26299f26b00901a008c25b6d0c
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2019-01-05 15:34:03 +0100

    Adding componentDidMount

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import './App.css';
 import Intro from '../intro/index';
 
@@ -7,15 +9,29 @@ class App extends Component {
     series: []
   }
 
+  componentDidMount() {
+    const series = ["Vikings", "Game sof Thrones","Wataha"];
+    this.setState({
+      series: series
+    });
+    setTimeout(() => {
+
+    }, 2000);
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">TV Series list</h1>
-        </header>
-        <Intro message="Here you can find all of your most loved series" />
-        The length of series array - {this.state.series.length}
-      </div>
+    return ( <
+      div className = "App" >
+      <
+      header className = "App-header" >
+      <
+      h1 className = "App-title" > TV Series list < /h1> <
+      /header> <
+      Intro message = "Here you can find all of your most loved series" / >
+      The length of series array - {
+        this.state.series.length
+      } <
+      /div>
     );
   }
 }
