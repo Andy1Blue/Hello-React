c58943d52f388454d463bec1a48dda6ab8f27599
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2019-01-05 15:39:16 +0100

    Installing fetch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import React, {
 } from 'react';
 import './App.css';
 import Intro from '../intro/index';
+import 'whatwg-fetch';
 
 class App extends Component {
   state = {
@@ -10,28 +11,19 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const series = ["Vikings", "Game sof Thrones","Wataha"];
-    this.setState({
-      series: series
-    });
-    setTimeout(() => {
-
-    }, 2000);
+    
   }
 
   render() {
-    return ( <
-      div className = "App" >
-      <
-      header className = "App-header" >
-      <
-      h1 className = "App-title" > TV Series list < /h1> <
-      /header> <
-      Intro message = "Here you can find all of your most loved series" / >
+    return ( <div className = "App" >
+      <header className = "App-header">
+      <h1 className = "App-title" > TV Series list </h1> 
+      </header> 
+      <Intro message = "Here you can find all of your most loved series" />
       The length of series array - {
         this.state.series.length
-      } <
-      /div>
+      } 
+      </div>
     );
   }
 }
