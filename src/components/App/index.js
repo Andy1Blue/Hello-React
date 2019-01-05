69c6170e3684d52968845442352d5e51da9079c4
Krzysztof Bujak <34073209+Andy1Blue@users.noreply.github.com>
2019-01-05 15:49:43 +0100

    Adding series component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,30 +1,17 @@
-import React, {
-  Component
-} from 'react';
-import './App.css';
-import Intro from '../intro/index';
+import React, { Component} from 'react';
 import 'whatwg-fetch';
+import './App.css';
+import Intro from '../intro';
+import Series from '../../containers/Series';
 
 class App extends Component {
-  state = {
-    series: []
-  }
-
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-      .then(response => response.json())
-      .then(json => this.setState({series: json}));
-  }
-
   render() {
     return ( <div className = "App" >
       <header className = "App-header">
       <h1 className = "App-title" > TV Series list </h1> 
       </header> 
       <Intro message = "Here you can find all of your most loved series" />
-      The length of series array - {
-        this.state.series.length
-      } 
+      <Series />
       </div>
     );
   }
